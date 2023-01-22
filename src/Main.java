d76d50c35e208acf4c3ee18cb18cca1ae80eb815
Antony <officialantony23@gmail.com>
2023-01-22 21:04:27 +0300

    Speed converter

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609);
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        long miles = 0;
+        if (kilometersPerHour < 0)
+            System.out.println("Invalid value");
+        else {
+            miles = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = "+ miles + " mi/h");
+        }
+    }
+
+    public static void main(String[] args) {
+        printConversion(100);
+    }
+}
